c187c8799be9a50f8b2b49cfa3cc5b6232cd180b
AndreiB <up845111@myport.ac.uk>
2020-02-28 11:57:26 +0000

    week5 functional

---
+++ Week5.hs
@@ -0,0 +1,49 @@
+{- Week5.hs
+ This file illustrates list patterns and recursion over lists.
+-}
+
+import Prelude hiding (fst, snd, head, tail, sum, concat, reverse, zip)
+
+-- Definitions of the prelude functions fst and snd
+
+fst (x,_)       = x
+snd (_,y)       = y
+
+-- Definitions of the prelude functions head and tail
+
+head (x:_)      = x
+tail (_:xs)     = xs
+
+absFirst :: [Int] -> Int
+absFirst []     = -1
+absFirst (x:xs) = abs x
+
+sum :: [Int] -> Int
+sum []     = 0
+sum (x:xs) =   x + sum xs
+
+doubleAll :: [Int] -> [Int]
+doubleAll []      = []
+doubleAll (x:xs)  = 2*x : doubleAll xs
+
+concat :: [[a]] -> [a]
+concat []         = []
+concat (x:xs)     = x ++ concat xs
+
+reverse :: [a] -> [a]
+reverse []      = []
+reverse (x:xs)  = reverse xs ++ [x]
+
+zip :: [a] -> [b] -> [(a,b)]
+zip (x:xs) (y:ys)  = (x,y) : zip xs ys
+zip _ _            = []
+
+--1
+headPlusOne :: [Int] -> Int
+headPlusOne []     = 0
+headPlusOne (x:xs) = x + 1
+
+--2
+duplicateHead :: [a] -> [a]
+duplicateHead []     = []
+duplicateHead (x:xs) = [x] ++ duplicateHead x:xs
